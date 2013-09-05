b3fbc8b2580c9853786ccd5eb4332d6476aa159a
sillybean <sillybean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-05 19:15:29 +0000

    Fixes a bug that caused the date to appear outside its heading tag
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-with-excerpts/trunk@767514 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recent-with-excerpts-widgets.php
+++ recent-with-excerpts-widgets.php
@@ -4,7 +4,7 @@ Plugin Name: Recent Posts with Excerpts
 Plugin URI: http://stephanieleary.com/code/wordpress/recent-posts-with-excerpts/
 Donate link: http://stephanieleary.com/code/wordpress/recent-posts-with-excerpts/
 Description: A widget that lists your most recent posts with excerpts. The number of posts and excerpts is configurable; for example, you could show five posts but include the excerpt for only the most recent. Supports <a href="http://robsnotebook.com/the-excerpt-reloaded/">The Excerpt Reloaded</a> and <a href="http://sparepencil.com/code/advanced-excerpt/">Advanced Excerpt</a>.
-Version: 2.5
+Version: 2.5.1
 Author: Stephanie Leary
 Author URI: http://stephanieleary.com
 
@@ -89,7 +89,7 @@ class RecentPostsWithExcerpts extends WP_Widget {
                 echo '<h2'.$h2_classes.'><a href="'.get_permalink().'">'.get_the_title().'</a></h2>';
 				
 				if (!empty($date)) 
-					echo '<h3 class="date">'.the_time($date).'</h3>';
+					echo '<h3 class="date">'.get_the_time($date).'</h3>';
                 
                 if ($excerpts > 0) { // show the excerpt 
 					if ($instance['thumb'] && $instance['thumbposition'] == 'between')
